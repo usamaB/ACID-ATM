56e81e77cdd140d7f2b107f7a132a85ec24c8906
usamaB <usama.kaleem88@gmail.com>
2018-10-30 01:42:53 +0100

    add locks on the queries

--- src/main/java/ATMTransactions.java
+++ src/main/java/ATMTransactions.java
@@ -1,11 +1,10 @@
-package com.tuberlin.EC;
+
 
 import java.sql.*;
 
 public class ATMTransactions {
 
     public Boolean login(Connection conn, int accountId, int pinCode){
-//        Connection conn = getConnection();
         Statement stmt = null;
         String query = "SELECT * FROM account WHERE account_id=" + accountId + " AND pinCode=" + pinCode;
         try {
@@ -13,46 +12,45 @@ public class ATMTransactions {
             ResultSet rs = stmt.executeQuery(query);
             if(rs.next()){
                 System.out.println("Login Successful");
+                stmt.close();
                 return true;
             } else{
                 System.out.println("Invalid Credentials, try again");
+                stmt.close();
                 return false;
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
         return false;
     }
 
     public double checkBalance(Connection conn, int accountId){
-//        if(login(accountId,pinCode)){
-//            Connection conn = getConnection();
             Statement stmt = null;
             String query = "SELECT balance FROM account WHERE account_id=" + accountId;
             try {
                 stmt = conn.createStatement();
                 ResultSet rs = stmt.executeQuery(query);
+                rs.next();
+                stmt.close();
                 return rs.getDouble(1);
 
             } catch (SQLException e) {
                 e.printStackTrace();
             }
-//        }
         return -1;
     }
 
     public void withdrawAmount(Connection conn, int accountId, double amount){
-//        Connection conn = getConnection();
         Statement stmt = null;
         String query = null;
-//        if(login(accountId,pinCode)){
             try {
                 DatabaseMetaData db = conn.getMetaData();
-                System.out.println("Transaction Isolation level= " + conn.getTransactionIsolation());
                 if(db.supportsTransactionIsolationLevel(Connection.TRANSACTION_SERIALIZABLE))
                     conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
 
-                query = "SELECT balance FROM account WHERE account_id=" + accountId;
+                query = "SELECT balance FROM account WHERE account_id=" + accountId + " FOR UPDATE";
 
                 stmt = conn.createStatement();
                 ResultSet rs = stmt.executeQuery(query);
@@ -60,16 +58,15 @@ public class ATMTransactions {
                 double balance =  rs.getDouble(1);
 
                 String update = null;
-                if(balance > amount){
-                    update = "Update amount SET balance=" +(balance-amount) + " WHERE account_id=" + accountId;
+                if(balance < amount){
+
+                    System.out.println("Balance less than the withdrawl amount");
+                    return;
                 }
 
-                //enable transaction demarcation
+                update = "Update account SET balance=" +(balance-amount) + " WHERE account_id=" + accountId;
                 conn.setAutoCommit(false);
                 stmt = conn.createStatement();
-
-                //submit transaction (prepare)
-                System.out.println("Submit demarcated transactions to database");
                 stmt.executeUpdate(update);
 
                 query = "SELECT balance FROM account WHERE account_id=" + accountId;
@@ -85,64 +82,42 @@ public class ATMTransactions {
                     System.out.println("Commit demarcated transactions.");
                     conn.commit();
                 }
+                stmt.close();
             } catch (SQLException e){
                 e.printStackTrace();
             }
-//        }
-
         return;
     }
 
     public void depositAmount(Connection conn, int accountId, double amount){
-//        Connection conn = getConnection();
         Statement stmt = null;
         String query = null;
 
-//        if(login(accountId,pinCode)){
             try {
                 DatabaseMetaData db = conn.getMetaData();
-                System.out.println("Transaction Isolation level= " + conn.getTransactionIsolation());
                 if(db.supportsTransactionIsolationLevel(Connection.TRANSACTION_SERIALIZABLE))
                     conn.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);
 
-                query = "SELECT balance FROM account WHERE account_id=" + accountId;
+                query = "SELECT balance FROM account WHERE account_id=" + accountId + " FOR UPDATE";
 
+                conn.setAutoCommit(false);
                 stmt = conn.createStatement();
                 ResultSet rs = stmt.executeQuery(query);
                 rs.next();
                 double balance =  rs.getDouble(1);
 
                 String update = null;
-//                if(balance > amount){
-                update = "Update amount SET balance=" +(balance+amount) + " WHERE account_id=" + accountId;
-//                }
-
-                //enable transaction demarcation
-//                conn.setAutoCommit(false);
+                update = "UPDATE account SET balance=" +(balance+amount) + " WHERE account_id=" + accountId;
                 stmt = conn.createStatement();
-
-                //submit transaction (prepare)
-//                System.out.println("Submit demarcated transactions to database");
                 stmt.executeUpdate(update);
-
-//                query = "SELECT balance FROM account WHERE account_id=" + accountId;
-//                stmt = conn.createStatement();
-//                rs = stmt.executeQuery(query);
-//                rs.next();
-//                balance =  rs.getDouble(1);
-//                if(balance<0) {
-//                    System.out.println("The transaction not complteted, Rolling back");
-//                    conn.rollback();
-//                } else {
-//                    //Commit both transactions.
-//                    System.out.println("Commit demarcated transactions.");
-//                    conn.commit();
-//                }
+                stmt.close();
             } catch (SQLException e){
                 e.printStackTrace();
             }
-//        }
-
         return;
+    }
+
+    public void close(){
+        System.exit(0);
     }
 }
--- src/main/java/DBSetup.java
+++ src/main/java/DBSetup.java
@@ -1,4 +1,3 @@
-package com.tuberlin.EC;
 
 
 import java.sql.*;
@@ -6,7 +5,7 @@ import java.util.Scanner;
 
 public class DBSetup {
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-    static final String DB_URL = "jdbc:mysql://ecdbinstance.cazdtabk6sqg.eu-central-1.rds.amazonaws.com/testdb";
+    static final String DB_URL = "jdbc:mysql://testdb.cazdtabk6sqg.eu-central-1.rds.amazonaws.com/testdb";
     static final String USER = "sam";
     static final String PASS = "sambutt88";
 
@@ -17,73 +16,78 @@ public class DBSetup {
         checkDriver();
         conn = getConnection();
 
-//        Scanner scanner = new Scanner(System.in);
-//        ATMTransactions atm = new ATMTransactions();
-//        System.out.println("Enter AccountID and PIN");
-//
-//        int accountId = scanner.nextInt();
-//        int pinCode = scanner.nextInt();
-//        boolean isLoggedIn = atm.login(conn,accountId,pinCode);
-//
-//        boolean play = true;
-//        while(play){
-//
-//            System.out.println("Press\n 1) for Show Balance \n" +
-//                    "2) for Deposit \n" +
-//                    "3) for Withdraw \n" +
-//                    "4) for exit");
-//
-//            switch(scanner.nextInt()){
-//                case 1:
-//                    atm.checkBalance(conn,accountId);
-//                case 2:
-//                    System.out.println("Enter the amount you want to deposit");
-//                    atm.depositAmount(conn,accountId,scanner.nextInt());
-//                case 3:
-//                    System.out.println("Enter the amount you want to deposit");
-//                    atm.withdrawAmount(conn,accountId,scanner.nextInt());
-//                case 4:
-//                    System.out.println("Exiting...");
-//                    return;
-//            }
-//        }
+        Scanner scanner = new Scanner(System.in);
+        ATMTransactions atm = new ATMTransactions();
+        System.out.println("Enter AccountID and PIN");
 
-        System.out.println("Creating statement...");
-        stmt = conn.createStatement();
-        String sql;
-        System.out.println("Creating table...");
-        sql = 	"CREATE TABLE IF NOT EXISTS account (account_id INT, pinCode INT, balance DECIMAL, UNIQUE (account_id));";
-        stmt.executeUpdate(sql);
+        int accountId = scanner.nextInt();
+        int pinCode = scanner.nextInt();
+        boolean isLoggedIn = atm.login(conn,accountId,pinCode);
 
-        System.out.println("Inserting values...");
-        sql = "INSERT INTO account VALUES(1, 1111, 50), (2, 2222, 50), (3,3333,50)";
-        stmt.executeUpdate(sql);
+        boolean play = true;
+        while(play){
 
-        //TRANSACTION DEMARCATION
-        //money transfer: send 10 from account 1 to account 2
-        String update_1 = "UPDATE account SET balance=40 WHERE account_id=1";
-        String update_2 = "UPDATE account SET balance=60 WHERE account_id=2";
+            System.out.println("Press\n" +
+                    "1) for Show Balance \n" +
+                    "2) for Deposit \n" +
+                    "3) for Withdraw \n" +
+                    "4) for exit");
 
-        //enable transaction demarcation
-        conn.setAutoCommit(false);
-        stmt = conn.createStatement();
-
-        try {
-            //submit transaction (prepare)
-            System.out.println("Submit demarcated transactions to database");
-            stmt.executeUpdate(update_1);
-            stmt.executeUpdate(update_2);
-
-            //Commit both transactions.
-            System.out.println("Commit demarcated transactions.");
-            conn.commit();
-        } catch (SQLException e) {
-            System.out.println("Rollback demarcated transactions.");
-            conn.rollback();
+            switch(scanner.nextInt()){
+                case 1:
+                    System.out.println("You current balance is " + atm.checkBalance(conn,accountId));
+                    break;
+                case 2:
+                    System.out.println("Enter the amount you want to deposit");
+                    atm.depositAmount(conn,accountId,scanner.nextInt());
+                    break;
+                case 3:
+                    System.out.println("Enter the amount you want to deposit");
+                    atm.withdrawAmount(conn,accountId,scanner.nextInt());
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    atm.close();
+                    return;
+            }
         }
 
-        System.out.println("Closing connection...");
-        stmt.close();
+//        System.out.println("Creating statement...");
+//        stmt = conn.createStatement();
+//        String sql;
+//        System.out.println("Creating table...");
+//        sql = 	"CREATE TABLE IF NOT EXISTS account (account_id INT, pinCode INT, balance DECIMAL, UNIQUE (account_id));";
+//        stmt.executeUpdate(sql);
+//
+//        System.out.println("Inserting values...");
+//        sql = "INSERT INTO account VALUES(1, 1111, 50), (2, 2222, 50), (3,3333,50)";
+//        stmt.executeUpdate(sql);
+//
+//        //TRANSACTION DEMARCATION
+//        //money transfer: send 10 from account 1 to account 2
+//        String update_1 = "UPDATE account SET balance=40 WHERE account_id=1";
+//        String update_2 = "UPDATE account SET balance=60 WHERE account_id=2";
+//
+//        //enable transaction demarcation
+//        conn.setAutoCommit(false);
+//        stmt = conn.createStatement();
+//
+//        try {
+//            //submit transaction (prepare)
+//            System.out.println("Submit demarcated transactions to database");
+//            stmt.executeUpdate(update_1);
+//            stmt.executeUpdate(update_2);
+//
+//            //Commit both transactions.
+//            System.out.println("Commit demarcated transactions.");
+//            conn.commit();
+//        } catch (SQLException e) {
+//            System.out.println("Rollback demarcated transactions.");
+//            conn.rollback();
+//        }
+//
+//        System.out.println("Closing connection...");
+//        stmt.close();
     }
 
     private static void checkDriver(){
